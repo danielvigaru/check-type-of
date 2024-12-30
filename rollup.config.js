254cbdaca2684ac8c9ca8176e69d311330a79c2e
Daniel Vigaru <22689627+danielvigaru@users.noreply.github.com>
2024-12-30 23:06:04 +0200

    chore: minify build

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,18 @@
+import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 
 export default {
     input: 'src/index.ts',
     output: [
         {
-            file: 'lib/index.cjs',
-            format: 'cjs',
+            file: 'lib/index.umd.js',
+            format: 'umd',
+            name: 'CheckType',
         },
         {
             file: 'lib/index.mjs',
             format: 'es',
         },
     ],
-    plugins: [typescript()],
+    plugins: [typescript(), terser()],
 };
